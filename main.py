3bf00a26875dca100ee2fa20d544a7518c81ddca
Githubtesi <tesiyosi7211@gmail.com>
2020-06-29 03:32:51 +0900

    taxi_cab_numberの追加

--- main.py
+++ main.py
@@ -2,7 +2,16 @@
 # Input 1,2 [(1729,[(1,12),(9,10)])]
 # Input 2,2 [(1729,[(1,12),(9,10)]) ,(4104,[(2,16),(9,15)])]
 # Input 1,3 [(87539319,[(167,436),(228,423),(255,414)])]
+from collections import defaultdict
+from typing import List, Tuple
 
+
+def taxi_cab_number(max_answer_num: int, match_answer_num: int = 2) -> List[Tuple[int, List[Tuple[int, int]]]]:
+    result = []
+    got_answer_count = 0
+    while got_answer_count < max_answer_num:
+        match_answer_count = 0
+        memo = defaultdict(list)
 
 
 if __name__ == '__main__':
